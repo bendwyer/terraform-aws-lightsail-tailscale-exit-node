c5736c9816dc52eec0aa63dff9229f69a82f59c3
bendwyer <17102207+bendwyer@users.noreply.github.com>
2025-03-09 19:40:14 +0000

    breaking: Remove unused providers

--- versions.tf
+++ versions.tf
@@ -1,17 +1,9 @@
 terraform {
-  required_version = ">=1.1.0"
+  required_version = ">=1.10.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">=5.37.0"
-    }
-    tailscale = {
-      source  = "tailscale/tailscale"
-      version = ">=0.13.13"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">=0.10.0"
     }
   }
 }
